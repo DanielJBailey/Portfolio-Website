acbb0db778d6d412ae8cea633d8043ffe0253e1c
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-15 18:25:06 -0700

    add styled components for home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,63 @@
 import React from 'react';
-// import Overlay from './Overlay';
-import '../styles/app.scss';
+import styled from 'styled-components';
 
-class Home extends React.Component {
+const Home = () => (
+    <Container>
+        <Menu>Menu</Menu>
+        <Line></Line>
+        <Title>Software Engineer | SLC, Utah</Title>
+        <Name>Daniel Bailey</Name>
+    </Container>
+)
 
-    state = {
-        overlayOpen: true,
-        currentPage: 1
-    }
+const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    align-items: flex-start;
+    height: 100%;
+    min-height: 100vh;
+    width: 100%;
+    padding: 1em;
+    position: relative;
+`;
 
-    toggleOverlay = () => {
-        console.log('made it');
-        let { overlayOpen } = this.state;
-        this.setState({
-            overlayOpen: !overlayOpen
-        })
-    }
+const Name = styled.h1`
+    font-size: 4em;
+    letter-spacing: -2px;
+    margin-left: -4px;
+    line-height: 1.0;
+    font-family: 'Catamaran', sans-serif;
+`;
 
-    render() {
-        // let {overlayOpen} = this.state;
-        return (
-            <>
-                {/* <Overlay open={overlayOpen} toggle={this.toggleOverlay}/> */}
-                <div className="open-menu">
-                    menu
-                    </div>
-                <section id="home" className="section">
-                    <h1 className="name">Daniel Bailey</h1>
-                    <h3 className="title">Software Engineer <span style={{ color: "#A9BCD0" }}>|</span> SLC, Utah</h3>
-                </section>
-            </>
-        )
-    }
+const Title = styled.h1`
+    line-height: 1.0;
+    font-family: 'Catamaran', sans-serif;
+    font-size: 1.25em;
+    margin-top: 5px;
+`;
 
-}
+const Menu = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 1em;
+    font-weight: 400;
+    font-family: 'Catamaran', sans-serif;
+    font-size: 1.25em;
+`;
+
+const Line = styled.hr`
+    width: 50%;
+    height: 10px;
+    background-color: rgba(0,0,0,0.7);
+    margin: 10px 0;
+    border: none;
+    outline: none;
+`;
 
 export default Home;
