ba7ee3f57393ca23a9edb96464d13bcfb47da99b
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-06 23:35:12 -0700

    add components for pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
+import {Switch, Route} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import Work from './components/Work';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-      </>
-    );
-  }
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/about" component={About} />
+    <Route exact path="/work" component={Work} />
+    <Route exact path="/experience" component={Experience} />
+    <Route exact path="/contact" component={Contact} />
+  </Switch>
+)
 
 export default App;
