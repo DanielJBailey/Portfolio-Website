ae03707cc8564481d8353d966bd86208abd3e689
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-07 13:50:43 -0700

    move menu to app to keep code dry

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,76 @@ import About from './components/About/About';
 import Work from './components/Work/Work';
 import Experience from './components/Experience/Experience';
 import Contact from './components/Contact/Contact';
+import SideMenu from './components/SideMenu';
+import styled from 'styled-components';
 import './App.scss';
 
-const App = () => (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/work" component={Work} />
-      <Route exact path="/experience" component={Experience} />
-      <Route exact path="/contact" component={Contact} />
-    </Switch>
-)
+
+class App extends React.Component {
+
+  state={menuOpen: false};
+
+  toggleMenu = () => {
+    this.setState(state => ({menuOpen: !state.menuOpen}))
+  }
+
+  render() {
+    let {menuOpen} = this.state;
+    let className = menuOpen ? "menu-bar open" : "menu-bar";
+    let firstChildTransitions = ["translateY(10px)", "rotate(225deg)"];
+    let lastChildTransitions = ["translateY(-10px)", "rotate(-225deg)"];
+    return(
+      <>
+        <OpenMenu onClick={this.toggleMenu}>
+          <span className={className} animations={firstChildTransitions}></span>
+          <span className={className}></span>
+          <span className={className} animations={lastChildTransitions}></span>
+        </OpenMenu>
+        {menuOpen ? <SideMenu toggleMenu={this.toggleMenu} open={menuOpen}/> : null}
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/work" component={Work} />
+          <Route exact path="/experience" component={Experience} />
+          <Route exact path="/contact" component={Contact} />
+        </Switch>
+      </>
+    )
+  }
+}
+
+const OpenMenu = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    margin: 1em;
+    padding: 1em;
+    cursor: pointer;
+    z-index: 9999;
+    .menu-bar {
+        display: block;
+        width: 50px;
+        height: 2px;
+        margin-top: 8px;
+        border-radius: 2px;
+        background-color: black;
+        transition: 0.75s;
+    }
+    .menu-bar.open {
+        &:first-child {
+            transform: ${props => props.children[0].props.animations.map(animation => {
+                return animation
+            })}
+        }
+        &:nth-child(2) {
+            opacity: 0;
+        }
+        &:last-child {
+            transform: ${props => props.children[2].props.animations.map( animation => {
+                return animation
+            })}
+        }
+    }
+`;
 
 export default App;
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,98 +1,43 @@
-import React from 'react';
-import styled from 'styled-components';
-import SideMenu from './SideMenu';
+import React from "react";
+import styled from "styled-components";
 
-class Home extends React.Component {
-    state={menuOpen: false};
-
-    toggleMenu = () => {
-        this.setState(state => ({menuOpen: !state.menuOpen}))
-    }
-
-    render() {
-        let {menuOpen} = this.state;
-        let className = menuOpen ? "menu-bar open" : "menu-bar";
-        let firstChildTransitions = ["translateY(10px)", "rotate(225deg)"];
-        let lastChildTransitions = ["translateY(-10px)", "rotate(-225deg)"];
-        return(
-            <HomeContainer>
-                <OpenMenu onClick={this.toggleMenu}>
-                    <span className={className} animations={firstChildTransitions}></span>
-                    <span className={className}></span>
-                    <span className={className} animations={lastChildTransitions}></span>
-                </OpenMenu>
-                {menuOpen ? <SideMenu toggleMenu={this.toggleMenu} open={menuOpen}/> : null}
-                <Name>Daniel Bailey</Name>
-                <Title>Software Engineer | Salt Lake City, UT</Title>
-            </HomeContainer>
-        )
-    }
-}
-
-const OpenMenu = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    margin: 1em;
-    padding: 1em;
-    cursor: pointer;
-    z-index: 9999;
-    .menu-bar {
-        display: block;
-        width: 50px;
-        height: 2px;
-        margin-top: 8px;
-        border-radius: 2px;
-        background-color: black;
-        transition: 0.75s;
-    }
-    .menu-bar.open {
-        &:first-child {
-            transform: ${props => props.children[0].props.animations.map(animation => {
-                return animation
-            })}
-        }
-        &:nth-child(2) {
-            opacity: 0;
-        }
-        &:last-child {
-            transform: ${props => props.children[2].props.animations.map( animation => {
-                return animation
-            })}
-        }
-    }
-`;
+const Home = () => (
+  <HomeContainer>
+    <Name>Daniel Bailey</Name>
+    <Title>Software Engineer | Salt Lake City, UT</Title>
+  </HomeContainer>
+);
 
 const Name = styled.h1`
-    font-size: 55px;
-    font-family: 'Staatliches', cursive;
-    letter-spacing: 2px;
-    color: #222f3e;
-    line-height: 1.0;
-    @media (max-width: 325px) {
-        font-size: 50px;
-    }
+  font-size: 55px;
+  font-family: "Staatliches", cursive;
+  letter-spacing: 2px;
+  color: #222f3e;
+  line-height: 1;
+  @media (max-width: 325px) {
+    font-size: 50px;
+  }
 `;
 
 const Title = styled.p`
-    font-size: 16px;
-    line-height: 1.0;
-    color: #222f3e;
-    font-family: 'Sarabun', sans-serif;
-    @media (max-width: 325px) {
-        font-size: 14px;
-    }
+  font-size: 16px;
+  line-height: 1;
+  color: #222f3e;
+  font-family: "Sarabun", sans-serif;
+  @media (max-width: 325px) {
+    font-size: 14px;
+  }
 `;
 
 const HomeContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    justify-content: flex-end;
-    height: 100%;
-    min-height: 100vh;
-    width: 100%;
-    padding: 2em;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-end;
+  height: 100%;
+  min-height: 100vh;
+  width: 100%;
+  padding: 2em;
 `;
 
 export default Home;
