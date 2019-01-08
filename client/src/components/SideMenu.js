48b9af3365f00ad273bee2b34efb8dda561f572d
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-07 23:25:47 -0700

    work on about

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -59,6 +59,7 @@ const Overlay = styled.div`
   position: absolute;
   top: 0;
   left: 0;
+  z-index: 2;
 `;
 
 const MenuList = styled.ul`
--- client/src/components/Work/Work.js
+++ client/src/components/Work/Work.js
@@ -1,9 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Work = () => (
-    <>
-        <h1>Work</h1>
-    </>
+    <WorkContainer>
+        <Project>1</Project>
+        <Project>2</Project>
+        <Project>3</Project>
+        <Project>4</Project>
+        <Project>5</Project>
+        <Project>6</Project>
+        <Project>7</Project>
+        <Project>8</Project>
+    </WorkContainer>
+        
 )
+
+const WorkContainer = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-row-start: 1;
+    grid-column-end: 3;
+    height: 100%;
+    min-height: 100vh;
+    width: 100vw;
+    margin-top: 100px;
+`;
+
+const Project = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
 
 export default Work;
