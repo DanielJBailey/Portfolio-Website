6e1de186e3170f80e5ac1c5ecbf60df42c6569f7
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-26 13:10:13 -0700

    start working on work section

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,43 +1,35 @@
 import React from "react";
 import styled, { keyframes } from "styled-components";
 
-class About extends React.Component {
-  state = { long: true };
-
-  handleChange = () => {
-    this.setState(state => ({ long: !state.long }));
-  };
-
-  render() {
-    return (
+const About = () => {
+   return (
       <AboutContainer>
-        <h1>About</h1>
-        <Choice />
-
-        <Bio>
-          Hi, my name is Daniel and I am a software engineer with a background
-          and education in finance. I am originally from Birmingham, Alabama,
-          but recently relocated to Salt Lake City, Utah to pursue my interest
-          in software engineering.
-        </Bio>
-        <Bio>
-          After working in the banking industry for a few years after college, I
-          decided to pursue an exciting and challenging career in software
-          development. I was honored to receive the Opportunity in Tech
-          Scholarship for Dev Point Labs, located in the heart of Salt Lake
-          City, UT. I am currently a student at Dev Point Labs and will graduate
-          from the University of Utah partnered program in February of 2019.
-        </Bio>
-        <Bio>
-          I have a passion for design as well as an even greater hunger for
-          development. I thrive in fast paced environments, surrounded by people
-          with similair interests. I get excited about helping others learn how
-          to code, and see them get excited about things they are able to build.
-          I love to be challenged every day whether it be a new algorithm
-          challenge or designing and developing an application with technology
-          I've never used before.
-        </Bio>
-        <SkillsList>
+         <h1>About</h1>
+         <Bio>
+            Hi, my name is Daniel and I am a software engineer with a background
+            and education in finance. I am originally from Birmingham, Alabama,
+            but recently relocated to Salt Lake City, Utah to pursue my interest
+            in software engineering.
+         </Bio>
+         <Bio>
+            After working in the banking industry for a few years after college,
+            I decided to pursue an exciting and challenging career in software
+            development. I was honored to receive the Opportunity in Tech
+            Scholarship for Dev Point Labs, located in the heart of Salt Lake
+            City, UT. I am currently a student at Dev Point Labs and will
+            graduate from the University of Utah partnered program in February
+            of 2019.
+         </Bio>
+         <Bio>
+            I have a passion for design as well as an even greater hunger for
+            development. I thrive in fast paced environments, surrounded by
+            people with similair interests. I get excited about helping others
+            learn how to code, and see them get excited about things they are
+            able to build. I love to be challenged every day whether it be a new
+            algorithm challenge or designing and developing an application with
+            technology I've never used before.
+         </Bio>
+         <SkillsList>
             <p>Tech I am currently using includes:</p>
             <Skill>- JavaScript</Skill>
             <Skill>- Ruby</Skill>
@@ -45,27 +37,25 @@ class About extends React.Component {
             <Skill>- Ruby on Rails</Skill>
             <Skill>- MySQL/PostgreSQL</Skill>
             <Skill>- HTML/CSS/SASS</Skill>
-        </SkillsList>
-        <Bio>
-          In my free time you can usually find me snowboarding with friends,
-          playing video games, traveling all over the world, trying a new
-          restaurant, or exploring the outdoors with my amazing wife Jess and my
-          goofy golden retriever named Riggs.
-        </Bio>
-        
+         </SkillsList>
+         <Bio>
+            In my free time you can usually find me snowboarding with friends,
+            playing video games, traveling all over the world, trying a new
+            restaurant, or exploring the outdoors with my amazing wife Jess and
+            my goofy golden retriever named Riggs.
+         </Bio>
       </AboutContainer>
-    );
-  }
-}
+   );
+};
 
 const SkillsList = styled.ul`
-    width: 100%;
-    padding-top: 25px;
-    list-style: none; 
+   width: 100%;
+   padding-top: 25px;
+   list-style: none;
 `;
 
 const Skill = styled.li`
-    padding: 10px;
+   padding: 10px;
 `;
 
 const fadeIn = keyframes`
@@ -77,51 +67,41 @@ const fadeIn = keyframes`
     }
 `;
 
-const Choice = styled.span`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  align-self: flex-end;
-  z-index: 1;
-
-  p {
-    margin-left: 10px;
-  }
-`;
-
 const AboutContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: center;
-  height: 100%;
-  min-height: 100vh;
-  width: 100%;
-  padding: 2em;
-  animation: ${fadeIn} 1s linear;
+   display: flex;
+   flex-direction: column;
+   align-items: flex-start;
+   justify-content: center;
+   height: 100%;
+   min-height: 100vh;
+   width: 100%;
+   animation: ${fadeIn} 1s linear;
+   max-width: 600px;
+   width: 100%;
+   padding: 100px 2em;
+   margin: 0 auto;
 
-  h1 {
-    &:before {
-      display: block;
-      content: "";
-      width: 50px;
-      height: 10px;
-      background-color: #222f3e;
-    }
-  }
+   h1 {
+      &:before {
+         display: block;
+         content: "";
+         width: 50px;
+         height: 10px;
+         background-color: #222f3e;
+      }
+   }
 `;
 
 const Bio = styled.p`
-  line-height: 1.5;
-  padding-top: 25px;
+   line-height: 1.5;
+   padding-top: 25px;
 
-  img {
-    float: right;
-    @media (max-width: 425px) {
-      width: 100%;
-    }
-  }
+   img {
+      float: right;
+      @media (max-width: 425px) {
+         width: 100%;
+      }
+   }
 `;
 
 export default About;
--- client/src/components/Work/Work.js
+++ client/src/components/Work/Work.js
@@ -1,36 +1,119 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const Work = () => (
-    <WorkContainer>
-        <Project>1</Project>
-        <Project>2</Project>
-        <Project>3</Project>
-        <Project>4</Project>
-        <Project>5</Project>
-        <Project>6</Project>
-        <Project>7</Project>
-        <Project>8</Project>
-    </WorkContainer>
-        
-)
+   <WorkContainer>
+      <h1>My Current Work</h1>
+      <ProjectContainer>
+         <Project>
+            <ProjectTitle>Dev Tracker</ProjectTitle>
+            <Hr />
+            <ProjectDescription>
+               Dev Tracker is a web application that we built for Dev Point Labs
+               that allows students to log in and track current applications,
+               interviews, and phone calls for companies they are interested in.
+            </ProjectDescription>
+            <Features>
+               <Feature>
+                  <FeatureTitle>Application Tracking</FeatureTitle>
+                  <FeatureDescription>
+                     Users can add, update, and delete applications in order to
+                     keep track of multiple applications.
+                  </FeatureDescription>
+               </Feature>
+               <Feature>
+                  <FeatureTitle>Tracker</FeatureTitle>
+                  <FeatureDescription>
+                     Users can track applications in a list format and filter
+                     based on application status and can take advantage of a
+                     custom built search function, to even further filter
+                     results.
+                  </FeatureDescription>
+               </Feature>
+               <Feature>
+                  <FeatureTitle>Tasks</FeatureTitle>
+                  <FeatureDescription>
+                     Users can create tasks to be completed and schedule a due
+                     date to be completed by. Tasks are then displayed on
+                     dashboard to help remind users of upcoming tasks to be
+                     completed.
+                  </FeatureDescription>
+               </Feature>
+               <Feature>
+                  <FeatureTitle>Contacts</FeatureTitle>
+                  <FeatureDescription>
+                     Users can add contacts they have made for companies they
+                     are interested in. Users can input information such as
+                     links to their LinkedIn accounts or other contact
+                     information to help organize all information in one place.
+                  </FeatureDescription>
+               </Feature>
+               <Feature>
+                  <FeatureTitle>Event Monitoring</FeatureTitle>
+                  <FeatureDescription>
+                     Events were created with the goal of tracking progress with
+                     companies such as upcoming interviews, phone calls, or even
+                     offers our users have recieved.
+                  </FeatureDescription>
+               </Feature>
+            </Features>
+         </Project>
+         <Project />
+         <Project />
+      </ProjectContainer>
+   </WorkContainer>
+);
+
+const Feature = styled.li`
+   padding-top: 10px;
+`;
+const FeatureTitle = styled.h4``;
+const FeatureDescription = styled.p`
+   font-size: 14px;
+`;
+
+const Features = styled.ul`
+   list-style: none;
+   width: 100%;
+   padding: 1em 2em;
+`;
+
+const Hr = styled.hr`
+   width: 50%;
+   margin: 10px 0;
+`;
+
+const ProjectContainer = styled.div`
+   display: grid;
+   grid-template-columns: 1fr;
+   grid-gap: 1em;
+   padding: 50px 0;
+`;
 
 const WorkContainer = styled.div`
-    display: grid;
-    grid-template-columns: 1fr 1fr;
-    grid-row-start: 1;
-    grid-column-end: 3;
-    height: 100%;
-    min-height: 100vh;
-    width: 100vw;
+   height: 100%;
+   min-height: 100vh;
+   padding: 100px 2em;
+   max-width: 600px;
+   width: 100%;
+   margin: 0 auto;
+
+   h1 {
+      &:before {
+         display: block;
+         content: "";
+         width: 50px;
+         height: 10px;
+         background-color: #222f3e;
+      }
+   }
 `;
 
 const Project = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    background-color: #222f3e;
 `;
+
+const ProjectTitle = styled.h2``;
+
+const ProjectDescription = styled.p``;
 
 export default Work;
