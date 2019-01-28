1173e3aef89d1a8ce8861baf399140f21c7d1ef8
Daniel Bailey <daniel.justin.bailey@gmail.com>
2019-01-27 18:59:46 -0700

    Updates

---
+++ precache-manifest.9abcd287a4b27dff05ad3eb0dddb2ef0.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "1d8beca407405689f034",
+    "url": "/static/css/main.2f0af80a.chunk.css"
+  },
+  {
+    "revision": "1d8beca407405689f034",
+    "url": "/static/js/main.1d8beca4.chunk.js"
+  },
+  {
+    "revision": "46f081f2638b7602e724",
+    "url": "/static/js/1.46f081f2.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8468472aa8bdb5d7de284aef4742b123",
+    "url": "/static/media/dev_track_dashboard.8468472a.png"
+  },
+  {
+    "revision": "185dfd8bbcd77e8f00c2efd7db815b75",
+    "url": "/static/media/dev_track_contacts.185dfd8b.png"
+  },
+  {
+    "revision": "ff17dac2955c1deb4f1f972e2c374222",
+    "url": "/static/media/dev_track_login.ff17dac2.png"
+  },
+  {
+    "revision": "2f84ca89bfdbe7c2e5878f185c9ac6ba",
+    "url": "/static/media/dev_track_app.2f84ca89.png"
+  },
+  {
+    "revision": "d59a53ca7b5ba98b1125977109b079df",
+    "url": "/static/media/jeopardy_home.d59a53ca.png"
+  },
+  {
+    "revision": "09619eec94106b916fd7bd69f09ecb43",
+    "url": "/static/media/question.09619eec.png"
+  },
+  {
+    "revision": "bc04de7326f27e210890ed1a4a4cc5fe",
+    "url": "/static/media/game_board.bc04de73.png"
+  },
+  {
+    "revision": "fd4546ada9600471e2a3a3080b5bb176",
+    "url": "/static/media/admin_cat.fd4546ad.png"
+  },
+  {
+    "revision": "fa7bb38d28a6679a93c8a3e8e4de95c1",
+    "url": "/static/media/admin_questions.fa7bb38d.png"
+  },
+  {
+    "revision": "ea96ad6fddec04d8a5ea1daf892b9a02",
+    "url": "/static/media/trollo_dash.ea96ad6f.png"
+  },
+  {
+    "revision": "87e3c6bb0aa0077085ced26c38d17274",
+    "url": "/static/media/trollo_board.87e3c6bb.png"
+  },
+  {
+    "revision": "337bbf570dd890468d6af3bcf930e5a3",
+    "url": "/static/media/board_edit.337bbf57.png"
+  },
+  {
+    "revision": "718eb359d90485c629fb9fb862f2bf02",
+    "url": "/static/media/add_board.718eb359.png"
+  },
+  {
+    "revision": "781b384f0d8957d56c8335275df03d94",
+    "url": "/static/media/blog_home.781b384f.png"
+  },
+  {
+    "revision": "31d3533994c6a44c72fab594ebc0f5c8",
+    "url": "/static/media/blog_show.31d35339.png"
+  },
+  {
+    "revision": "757c84f05012f2d33082a922305541b0",
+    "url": "/static/media/blog_new.757c84f0.png"
+  },
+  {
+    "revision": "0512dbaf8d37ca2a62d78759243c2e1c",
+    "url": "/static/media/blog_body.0512dbaf.png"
+  },
+  {
+    "revision": "7e49db14847d65dc748851a611e2f428",
+    "url": "/static/media/sticky_notes.7e49db14.png"
+  },
+  {
+    "revision": "cb2f55590fd70fe1027ca1b691f65a5d",
+    "url": "/static/media/edit_sticky_note.cb2f5559.png"
+  },
+  {
+    "revision": "31a7c3d3a4d942aeb12c93f3492f74c8",
+    "url": "/index.html"
+  }
+];
